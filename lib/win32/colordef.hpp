572b40c53e364488fb1d98bd7e15c3f405a2039c
Luís Ferreira <luis_jorge_2@hotmail.com>
2016-04-06 08:09:01 +0000

    Update

--- lib/win32/colordef.hpp
+++ lib/win32/colordef.hpp
@@ -3,9 +3,9 @@
  * \______   \/   _____//   _____/  | BigSource Script
  *  |    |  _/\_____  \ \_____  \   | Project in C/C++ Language
  *  |    |   \/        \/        \  |
- *  |______  /_______  /_______  /  | @author Luís Ferreira
+ *  |______  /_______  /_______  /  | @author Luï¿½s Ferreira
  *         \/        \/        \/   | @license GNU Public License v3
- * Copyright (C) 2016 - Luís Ferreira. All right reserved
+ * Copyright (C) 2016 - Luï¿½s Ferreira. All right reserved
  * More information in: https://github.com/ljmf00/ (Github Page)
  */
 
@@ -23,6 +23,16 @@
 */
 
 #define WINSYSCOLOR_BLACKBLACK 0x00
-#define WINSYSCOLOR_BLACKBLUE 0X01
+#define WINSYSCOLOR_BLACKBLUE 0x01
+#define WINSYSCOLOR_BLACKGREEN 0x02
+#define WINSYSCOLOR_BLACKAQUA 0x03
+#define WINSYSCOLOR_BLACKRED 0x04
+#define WINSYSCOLOR_BLACKPURPLE 0x05
+#define WINSYSCOLOR_BLACKYELLOW 0x06
+#define WINSYSCOLOR_BLACKWHITE 0x07
+#define WINSYSCOLOR_BLACKGRAY 0x08
+#define WINSYSCOLOR_BALCKLIGHTBLUE 0x09
+#define WINSYSCOLOR_BLACKLIGHTGREEN 0x0A
+#define WINSYSCOLRO_BLACKLIGHTAQUA 0x0B
 
 #endif // COLORDEF_HPP_INCLUDED
